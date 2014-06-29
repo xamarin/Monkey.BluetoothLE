3bf921d9366576c3fc866c46ff26fa89dd9552ec
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2014-06-29 12:49:55 +0200

    Processing for .tinyfont files are implemented.

--- Source/MFMetaDataProcessor/MFMetaDataProcessor/MFMetaDataProcessor.Core/Tables/TinyResourceDataTable.cs
+++ Source/MFMetaDataProcessor/MFMetaDataProcessor/MFMetaDataProcessor.Core/Tables/TinyResourceDataTable.cs
@@ -15,6 +15,11 @@ namespace MFMetaDataProcessor
         private readonly IList<Byte[]> _dataByteArrays = new List<Byte[]>();
 
         /// <summary>
+        /// Gets current offset in resrouces data table (total size of all data blocks).
+        /// </summary>
+        public Int32 CurrentOffset { get; private set; }
+
+        /// <summary>
         /// Adds new chunk of binary data for resouces into list of resources.
         /// </summary>
         /// <param name="resourceData">Resouce data in binary format.</param>
@@ -22,6 +27,7 @@ namespace MFMetaDataProcessor
             Byte[] resourceData)
         {
             _dataByteArrays.Add(resourceData);
+            CurrentOffset += resourceData.Length;
         }
 
         /// <inheritdoc/>
@@ -32,6 +38,17 @@ namespace MFMetaDataProcessor
             {
                 writer.WriteBytes(item);
             }
+        }
+
+        /// <summary>
+        /// Aligns current data in table by word boundary and return size of alignment.
+        /// </summary>
+        /// <returns>Number of bytes added into bytes block for proper data alignment.</returns>
+        public Int32 AlignToWord()
+        {
+            var padding = (4 - (CurrentOffset % 4)) % 4;
+            AddResourceData(new Byte[padding]);
+            return padding;
         }
     }
 }
