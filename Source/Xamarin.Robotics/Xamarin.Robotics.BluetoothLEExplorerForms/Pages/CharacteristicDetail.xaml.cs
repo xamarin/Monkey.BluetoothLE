7467e9e2658de34857f0becdecb60328110e241d
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-07-02 19:26:32 -0700

    Xamarin.Forms CharacteristicDetail works (iOS)

--- Source/Xamarin.Robotics/Xamarin.Robotics.BluetoothLEExplorerForms/Pages/CharacteristicDetail.xaml.cs
+++ Source/Xamarin.Robotics/Xamarin.Robotics.BluetoothLEExplorerForms/Pages/CharacteristicDetail.xaml.cs
@@ -8,9 +8,28 @@ namespace Xamarin.Robotics.BluetoothLEExplorerForms
 {	
 	public partial class CharacteristicDetail : ContentPage
 	{	
+		IAdapter adapter;
+		IDevice device;
+		IService service; 
+		ICharacteristic characteristic;
+
 		public CharacteristicDetail (IAdapter adapter, IDevice device, IService service, ICharacteristic characteristic)
 		{
 			InitializeComponent ();
+			this.characteristic = characteristic;
+		}
+
+		protected override async void OnAppearing ()
+		{
+			base.OnAppearing ();
+		
+			var c = await characteristic.ReadAsync();
+
+			Name.Text = c.Name;
+			ID.Text = c.ID.ToString();
+			RawValue.Text = string.Join (",", c.Value);
+			StringValue.Text = c.StringValue;
+
 		}
 	}
 }
