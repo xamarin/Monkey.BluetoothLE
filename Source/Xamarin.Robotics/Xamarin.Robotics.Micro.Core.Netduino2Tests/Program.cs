0c4c9bbfff90bf57bb7be00b1f7c6682bf55dfc4
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-08-12 15:15:03 -0700

    Moved MotorShield code into separate cs file

--- Source/Xamarin.Robotics/Xamarin.Robotics.Micro.Core.Netduino2Tests/Program.cs
+++ Source/Xamarin.Robotics/Xamarin.Robotics.Micro.Core.Netduino2Tests/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SecretLabs.NETMF.Hardware.Netduino;
+using System;
 using System.Threading;
 
 namespace Xamarin.Robotics.Micro.Core.Netduino2Tests
@@ -7,6 +8,18 @@ namespace Xamarin.Robotics.Micro.Core.Netduino2Tests
 	{
 		public static void Main()
 		{
+            var led = new Microsoft.SPOT.Hardware.OutputPort(Pins.ONBOARD_LED, false);
+
+            for (var i = 0; i < 3; i++)
+            {
+                led.Write(true);
+                Thread.Sleep(250);
+                led.Write(false);
+                Thread.Sleep(250);
+            }
+
+            //TestDrunkenRobotWithMotorShield.Run();
+
             // TestSensors.Run ();
             // TestDrunkenRobot.Run ();
             // TestWallBouncingRobotWithHBridge.Run ();
--- Source/Xamarin.Robotics/Xamarin.Robotics.Micro.Core.Netduino2Tests/TestTwoWheeledRobot.cs
+++ Source/Xamarin.Robotics/Xamarin.Robotics.Micro.Core.Netduino2Tests/TestTwoWheeledRobot.cs
@@ -79,18 +79,6 @@ namespace Xamarin.Robotics.Micro.Core.Netduino2Tests
         }
     }
 
-    public class TestDrunkenRobotWithMotorShield
-    {
-        public static void Run ()
-        {
-            var shield = new AdafruitMotorShield ();
-            var robot = new TwoWheeledRobot (shield.GetMotor (1), shield.GetMotor (2));
-
-            new SineWave (0.1, 0.5, 0.5, updateFrequency: 9).Output.ConnectTo (robot.SpeedInput);
-            new SineWave (0.5, 0.333, 0, updateFrequency: 11).Output.ConnectTo (robot.DirectionInput);
-        }
-    }
-
     public class TestWallBouncingRobotWithHBridge
     {
         public static void Run ()
