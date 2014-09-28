24553eceb4f039447695f27a948fe1daf3500790
bryan costanich <bryancostanich@hotmail.com>
2014-09-27 20:38:06 -0700

    re-org'd the project to move the apps out of the platform stacks

---
+++ Source/Apps/Robotroller/Robotics.Mobile.Robotroller.Core/IGyro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robotics.Mobile.Robotroller
+{
+	public interface IGyro
+	{
+		double Roll { get; }
+		double Pitch { get; }
+		double Yaw { get; }
+		event EventHandler GyroUpdated;
+	}
+}
---
+++ Source/Apps/Robotroller/Robotics.Mobile.Robotroller.Core/Pages/DeviceList.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Robotics.Mobile.Core.Bluetooth.LE;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Robotics.Messaging;
+
+namespace Robotics.Mobile.Robotroller
+{	
+	public partial class DeviceList : ContentPage
+	{	
+		readonly IAdapter adapter;
+		ObservableCollection<IDevice> devices;
+
+		bool autoScan = false;
+
+		public DeviceList (IAdapter adapter)
+		{
+			InitializeComponent ();
+			this.adapter = adapter;
+			this.devices = new ObservableCollection<IDevice> ();
+			listView.ItemsSource = devices;
+
+			adapter.DeviceDiscovered += (object sender, DeviceDiscoveredEventArgs e) => {
+				Device.BeginInvokeOnMainThread(() => {
+					devices.Add (e.Device);
+				});
+			};
+
+			adapter.ScanTimeoutElapsed += (sender, e) => {
+				IsBusy = false;
+				Debug.WriteLine ("Scan timeout");
+				if (autoScan) {
+					StartScanning ();
+				}
+			};
+
+			Appearing += (sender, e) => {
+				StartScanning();
+			};
+		}
+
+		public void OnItemSelected (object sender, SelectedItemChangedEventArgs e)
+		{
+			if (((ListView)sender).SelectedItem == null) {
+				return;
+			}
+			StopScanning ();
+
+			var device = e.SelectedItem as IDevice;
+
+			var servicePage = new DeviceDetail(adapter, device.ID);
+			// load services on the next page
+			Navigation.PushAsync(servicePage);
+
+			((ListView)sender).SelectedItem = null; // clear selection
+		}
+
+		void StartScanning () {
+			if (!adapter.IsScanning) {
+				IsBusy = true;
+				devices.Clear();
+				adapter.StartScanningForDevices();
+				Debug.WriteLine ("adapter.StartScanningForDevices()");
+			}
+		}
+
+		void StopScanning () {
+			// stop scanning
+			new Task( () => {
+				if(adapter.IsScanning) {
+					Debug.WriteLine ("Still scanning, stopping the scan");
+					adapter.StopScanningForDevices();
+					IsBusy = false;
+				}
+			}).Start();
+		}
+	}
+}
