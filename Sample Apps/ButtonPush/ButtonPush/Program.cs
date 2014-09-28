e64bed06fdece119c11fef1350d3a8230584f097
bryan costanich <bryancostanich@hotmail.com>
2014-09-28 13:41:44 -0700

    initial check in of button push test app.

---
+++ Sample Apps/ButtonPush/ButtonPush/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.Threading;
+
+namespace ButtonPush
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			// configure an output port for us to "write" to the LED
+			OutputPort led = new OutputPort(Pins.ONBOARD_LED, false); 
+			// note that if we didn't have the SecretLabs.NETMF.Hardware.Netduino DLL, we could also manually access it this way:
+			//OutputPort led = new OutputPort(Cpu.Pin.GPIO_Pin10, false); 
+			int i = 0;
+			while (true) 
+			{ 
+				led.Write(true); // turn on the LED 
+				Thread.Sleep(250); // sleep for 250ms 
+				led.Write(false); // turn off the LED 
+				Thread.Sleep(250); // sleep for 250ms 
+
+				Debug.Print ("Looping" + i);
+				i++;
+			} 			
+		}
+	}
+}
