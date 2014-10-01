584f1efcc8e6372054fe0f2575bdc9090f3dcde6
bryan costanich <bryancostanich@hotmail.com>
2014-10-01 11:43:29 -0700

    updated button press app so it works.

--- Sample Apps/ButtonPush/ButtonPush/Program.cs
+++ Sample Apps/ButtonPush/ButtonPush/Program.cs
@@ -1,39 +1,41 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.SPOT;
 using H = Microsoft.SPOT.Hardware;
-using SecretLabs.NETMF.Hardware.Netduino;
-using System.Threading;
 using Robotics.Micro;
+using Robotics.Micro.Devices;
 using Robotics.Micro.Sensors.Buttons;
+using SecretLabs.NETMF.Hardware.Netduino;
 
 namespace ButtonPush
 {
 	public class Program
 	{
+		// create our pin references.
+		// note, when you create a reference to the onboard button, netduino is 
+		// smart enough to not use it as a reset button. :)
+		static H.Cpu.Pin buttonHardware = Pins.ONBOARD_BTN;
+		static H.Cpu.Pin ledHardware = Pins.ONBOARD_LED;
+
 		public static void Main()
 		{
-            PushButton button = new PushButton();
-            //button.ConnectTo(new DigitalInputPin(Pins.ONBOARD_BTN).Output);
-            InputPort buttonIn = new InputPort(button, "MyButton", Units.Digital);
-			//buttonIn.ConnectTo(Pins.ONBOARD_BTN);
-            H.OutputPort led = new H.OutputPort(Pins.ONBOARD_LED, false);
+			// Create the blocks
+			var button = new DigitalInputPin (buttonHardware);
+			var led = new DigitalOutputPin (ledHardware);
 
-			bool _isLedOn = false;
+			// Connect them together. with the block/scope architecture, you can think
+			// of everything as being connectable - output from one thing can be piped
+			// into another. in this case, we're setting the button output to the LED
+			// input. so when the user presses on the button, the signal goes straight
+			// to the LED.
+			button.Output.ConnectTo (led.Input);
 
-            button.Clicked += (s, e) => {
-				_isLedOn = !_isLedOn;
-				led.Write(_isLedOn);
-            };
+			// Do nothing
+			while (true) {
+				System.Threading.Thread.Sleep (1000);
+			}
 
-
-			while (true) 
-			{ 
-			} 			
 		}
 
-        static void button_Clicked(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
 	}
 }
