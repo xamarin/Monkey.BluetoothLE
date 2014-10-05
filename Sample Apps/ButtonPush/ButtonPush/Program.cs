5c61251c9645075997dcadc0c574977dd58aeefa
bryan costanich <bryancostanich@hotmail.com>
2014-10-04 17:35:20 -0700

    re-doing button push project, since it failed for no apparent reason

--- Sample Apps/ButtonPush/ButtonPush/Program.cs
+++ Sample Apps/ButtonPush/ButtonPush/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using Microsoft.SPOT;
 using H = Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
 using Robotics.Micro;
 using Robotics.Micro.Devices;
-using Robotics.Micro.Sensors.Buttons;
-using SecretLabs.NETMF.Hardware.Netduino;
 
 namespace ButtonPush
 {
@@ -20,19 +22,20 @@ namespace ButtonPush
 		public static void Main()
 		{
 			// Create the blocks
-			var button = new DigitalInputPin (buttonHardware);
-			var led = new DigitalOutputPin (ledHardware);
+			var button = new DigitalInputPin(buttonHardware);
+			var led = new DigitalOutputPin(ledHardware);
 
 			// Connect them together. with the block/scope architecture, you can think
 			// of everything as being connectable - output from one thing can be piped
 			// into another. in this case, we're setting the button output to the LED
 			// input. so when the user presses on the button, the signal goes straight
 			// to the LED.
-			button.Output.ConnectTo (led.Input);
+			button.Output.ConnectTo(led.Input);
 
-			// keep the program alive
-			while (true) {
-				System.Threading.Thread.Sleep (1000);
+			// keep the main loop alive so the program doesn't exit.
+			while (true)
+			{
+				Thread.Sleep(1000);
 			}
 		}
 	}
