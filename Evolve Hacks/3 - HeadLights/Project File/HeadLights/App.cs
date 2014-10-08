bd3a0235a4bb7fa8532a52e5e28e74916b71ebfd
bryan costanich <bryancostanich@hotmail.com>
2014-10-08 09:06:12 -0400

    initial check in of first hack

---
+++ Evolve Hacks/3 - HeadLights/Project File/HeadLights/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using Robotics.Mobile.Core.Bluetooth.LE;
+
+namespace HeadLights
+{
+	public class App
+	{
+		public static Page GetMainPage (IAdapter adapter)
+		{	
+			return new MainPage (adapter);
+		}
+	}
+}
+
