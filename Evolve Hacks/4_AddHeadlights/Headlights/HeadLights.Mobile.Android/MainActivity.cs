0fdbfd7353455b3ce81a7f64b0806f08721b33ca
bryan costanich <bryancostanich@hotmail.com>
2014-10-08 15:23:08 -0400

    adding headlights

--- Evolve Hacks/4_AddHeadlights/Headlights/HeadLights.Mobile.Android/MainActivity.cs
+++ Evolve Hacks/4_AddHeadlights/Headlights/HeadLights.Mobile.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-
+using Robotics.Mobile.Core.Bluetooth.LE;
 using Xamarin.Forms.Platform.Android;
 
 
@@ -22,7 +22,8 @@ namespace HeadLights.Android
 
 			Xamarin.Forms.Forms.Init (this, bundle);
 
-			SetPage (App.GetMainPage ());
+			//TODO: need adapter
+			//SetPage (App.GetMainPage ());
 		}
 	}
 }
