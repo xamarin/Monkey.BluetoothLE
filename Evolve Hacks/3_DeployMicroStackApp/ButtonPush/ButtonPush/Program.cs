e7a13c0c6e942155e732e17bd8280d0054626f39
bryan costanich <bryancostanich@hotmail.com>
2014-10-08 14:07:48 -0400

    Fixed binary reference on button push

---
+++ Evolve Hacks/3_DeployMicroStackApp/ButtonPush/ButtonPush/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using H = Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using Robotics.Micro;
+using Robotics.Micro.Devices;
+
+namespace ButtonPush
+{
+	public class Program
+	{
+		// create our pin references.
+		// note, when you create a reference to the onboard button, netduino is 
+		// smart enough to not use it as a reset button. :)
+		static H.Cpu.Pin buttonHardware = Pins.ONBOARD_BTN;
+		static H.Cpu.Pin ledHardware = Pins.ONBOARD_LED;
+
+		public static void Main()
+		{
+			// Create the blocks
+			var button = new DigitalInputPin(buttonHardware);
+			var led = new DigitalOutputPin(ledHardware);
+
+			// Connect them together. with the block/scope architecture, you can think
+			// of everything as being connectable - output from one thing can be piped
+			// into another. in this case, we're setting the button output to the LED
+			// input. so when the user presses on the button, the signal goes straight
+			// to the LED.
+			button.Output.ConnectTo(led.Input);
+
+			// keep the main loop alive so the program doesn't exit.
+			while (true)
+			{
+				Thread.Sleep(1000);
+			}
+		}
+	}
+}
---
+++ Evolve Hacks/4_AddHeadlights/Headlights/Headlights.Micro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO.Ports;
+using Robotics.Messaging;
+using Robotics.Micro.Devices;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Robotics.Micro.Core.Netduino2Tests
+{
+	public class TestHeadLights
+	{
+		public static void Main ()
+		{
+			// Create the blocks
+			var leftLight = new DigitalOutputPin (Pins.GPIO_PIN_D10) { Name = "LeftLight" };
+			var rightLight = new DigitalOutputPin (Pins.GPIO_PIN_D11) { Name = "RightLight" };
+
+			// Init
+			leftLight.Input.Value = 0;
+			rightLight.Input.Value = 0;
+
+			// Create the control server
+			var serialPort = new SerialPort (SerialPorts.COM3, 57600, Parity.None, 8, StopBits.One);
+			serialPort.Open ();
+			var server = new ControlServer (serialPort);
+
+			// Expose the left and right lights to the control server
+			leftLight.Input.ConnectTo (server, writeable: true);
+			rightLight.Input.ConnectTo (server, writeable: true);
+
+			// Do nothing
+			for (; ; ) {
+				System.Threading.Thread.Sleep (1000);
+			}
+		}
+	}
+}
