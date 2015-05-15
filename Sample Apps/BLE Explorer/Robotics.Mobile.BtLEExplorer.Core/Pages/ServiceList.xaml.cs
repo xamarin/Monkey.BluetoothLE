c538bb548244b3284513e3e512d7fd54e5a6e524
Steven Moy <github@stevenmoy.com>
2015-05-15 14:01:20 -0700

    Check to see if Connection/Disconnect event corresponds to the device in interest.
    * Robustify removing delegates upon device disconnection

--- Sample Apps/BLE Explorer/Robotics.Mobile.BtLEExplorer.Core/Pages/ServiceList.xaml.cs
+++ Sample Apps/BLE Explorer/Robotics.Mobile.BtLEExplorer.Core/Pages/ServiceList.xaml.cs
@@ -22,6 +22,7 @@ namespace Robotics.Mobile.BtLEExplorer
 			this.services = new ObservableCollection<IService> ();
 			listView.ItemsSource = services;
 			adapter.DeviceConnected += this.OnDeviceConnected;
+			adapter.DeviceDisconnected += this.OnDeviceDisconnected;
 			DisconnectButton.Activated += (sender, e) => {
 				adapter.DisconnectDevice (device);
 				Navigation.PopToRootAsync(); // disconnect means start over
@@ -50,11 +51,29 @@ namespace Robotics.Mobile.BtLEExplorer
 			((ListView)sender).SelectedItem = null; // clear selection
 		}
 
-		public void OnDeviceConnected(object sender, EventArgs args)
+		public void OnDeviceConnected(object sender, DeviceConnectionEventArgs args)
 		{
-			this.adapter.DeviceConnected -= this.OnDeviceConnected;
-			this.device.ServicesDiscovered += this.ServicesDiscovered;
-			this.device.DiscoverServices ();
+			if (args.Device == this.device) {
+				this.adapter.DeviceConnected -= this.OnDeviceConnected;
+				this.device.ServicesDiscovered += this.ServicesDiscovered;
+				this.device.DiscoverServices ();
+			}
+		}
+
+		public void OnDeviceDisconnected(object sender, DeviceConnectionEventArgs args)
+		{
+			if (args.Device == this.device) {
+				this.adapter.DeviceDisconnected -= this.OnDeviceDisconnected;
+				// For robustness reason, its possible to be disconnected from device
+				// right after adding the OnConnected delgate in constructor. 
+				// We do not want DeviceConnected delegate become a hidden bomb.
+				this.adapter.DeviceConnected -= this.OnDeviceConnected;
+				// For robustness reason, its possible we go from device
+				// connected to device disconnected without going through
+				// service discovered. We do not want the service discover
+				// delegate become a hidden bomb.
+				this.device.ServicesDiscovered -= this.ServicesDiscovered;
+			}
 		}
 
 		public void ServicesDiscovered(object sender, EventArgs args)
